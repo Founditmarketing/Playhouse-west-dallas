e96e4c95f021054872f981d7f5763c755e09e5b8
KylanGillam <kylan@founditmarketing.com>
2026-02-20 15:35:19 -0600

    chore: update hero section video to recently added version

--- components/hero-section.tsx
+++ components/hero-section.tsx
@@ -57,7 +57,7 @@ export function HeroSection() {
           >
             <video
               ref={videoRef}
-              src="/images/playhousesnewvideo.mp4"
+              src="/images/playhouse test. long 1.mp4"
               className={cn(
                 "w-full h-full object-cover transition-all duration-300",
                 isCustomFullscreen ? "object-contain max-w-full max-h-full" : ""
