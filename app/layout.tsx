7d1931c798f165820ca455080546fb731f6374ab
Jason Albright <jasonalbright@Jasons-Air-2.lan>
2026-04-10 15:52:52 -0500

    Update GTM container ID to GTM-K3RGGRMG

--- app/layout.tsx
+++ app/layout.tsx
@@ -44,14 +44,14 @@ export default function RootLayout({
             new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
             j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
             'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-            })(window,document,'script','dataLayer','GTM-P94CSWRD');
+            })(window,document,'script','dataLayer','GTM-K3RGGRMG');
           `}
         </Script>
       </head>
       <body className="font-sans antialiased">
         <noscript>
           <iframe
-            src="https://www.googletagmanager.com/ns.html?id=GTM-P94CSWRD"
+            src="https://www.googletagmanager.com/ns.html?id=GTM-K3RGGRMG"
             height="0"
             width="0"
             style={{ display: 'none', visibility: 'hidden' }}
